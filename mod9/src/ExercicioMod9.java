875998456cca1a2cabc5db6579894e979a2a7fe5
Erick Gustavo <erick_gustavo010@hotmail.com>
2022-10-03 16:41:04 -0300

    Exercicio mod 9

---
+++ mod9/src/ExercicioMod9.java
@@ -0,0 +1,15 @@
+public class ExercicioMod9 {
+
+    public static void main(String[] args) {
+
+        int valor = 10;
+
+        System.out.println(valor);
+
+        int numI = Integer.valueOf(valor);
+        
+        System.out.println(numI);
+
+    }
+
+}
