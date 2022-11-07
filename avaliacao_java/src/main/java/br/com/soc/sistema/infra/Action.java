c3cfa39227bff7353e63e6fc85b153a438a25797
Erick Gustavo <erick_gustavo010@hotmail.com>
2022-11-07 15:38:25 -0300

    delete

--- avaliacao_java/src/main/java/br/com/soc/sistema/infra/Action.java
+++
@@ -1,7 +0,0 @@
-package br.com.soc.sistema.infra;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class Action extends ActionSupport {
-	public static final String REDIRECT = "redirect";
-}
